304166e559d6c33b0080ea06bf48c1b5bb2b3676
Kirillmlk <kostakov.kiirill@yandex.by>
2024-10-13 21:48:37 +0300

    Test tables for order processing

---
+++ app/Models/Cart.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cart';
+
+    protected $fillable = [
+        'user_id',
+        'menu_id',
+        'quantity',
+    ];
+
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
+}
